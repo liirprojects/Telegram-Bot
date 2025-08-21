dcb2a35cc7a4d4c559a1c6261c804c09cefaf2c9
valeriiia2003 <leraspasibukhova@ukr.net>
2025-08-21 12:41:26 +0100

    Add CommandHandler as a base class for command classes

---
+++ TelegramBot.Infrastructure/Services/CommandHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using TelegramBot.Application.Interfaces;
+
+namespace TelegramBot.Infrastructure.Services;
+
+public abstract class CommandHandler : ICommandHandler
+{
+    public abstract string Command { get; }
+
+    /*  protected readonly IMessageSender _messageSender;
+
+    protected CommandHandler(IMessageSender messageSender)
+    {
+        _messageSender = messageSender;
+    }
+    */
+    public abstract Task HandleAsync(long chatId, string message, CancellationToken ct);
+}

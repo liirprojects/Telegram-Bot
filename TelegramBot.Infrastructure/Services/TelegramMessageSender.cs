5335d5b9e75d0ae3801e763f41ae5c98e51d8329
valeriiia2003 <leraspasibukhova@ukr.net>
2025-08-26 11:45:19 +0100

    Add TelegramMessageSender for sending messages

---
+++ TelegramBot.Infrastructure/Services/TelegramMessageSender.cs
@@ -0,0 +1,23 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using TelegramBot.Application.Interfaces;
+
+namespace TelegramBot.Infrastructure.Services;
+
+public class TelegramMessageSender : IMessageSender
+{
+    private readonly ITelegramBotClient _botClient;
+
+    public TelegramMessageSender(ITelegramBotClient telegramBotClient)
+    {
+        _botClient = telegramBotClient;
+    }
+    public Task SendTextAsync(long chatId, string message, CancellationToken cancellationToken, ParseMode parse = ParseMode.None)
+    {
+        return _botClient.SendMessage(
+            chatId, 
+            message, 
+            parseMode: parse,
+            cancellationToken: cancellationToken);
+    }
+}

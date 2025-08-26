de2178a6c52b044806009be40164dd9022fac8d2
valeriiia2003 <leraspasibukhova@ukr.net>
2025-08-26 12:35:17 +0100

    Edit CommandHandler

--- TelegramBot.Infrastructure/Services/CommandHandler.cs
+++ TelegramBot.Infrastructure/Services/CommandHandler.cs
@@ -7,12 +7,11 @@ public abstract class CommandHandler : ICommandHandler
 {
     public abstract string Command { get; }
 
-    /*  protected readonly IMessageSender _messageSender;
+    protected readonly IMessageSender _messageSender;
 
     protected CommandHandler(IMessageSender messageSender)
     {
         _messageSender = messageSender;
     }
-    */
     public abstract Task HandleAsync(long chatId, string message, CancellationToken ct);
 }

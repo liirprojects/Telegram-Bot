9426b5407f5cf8f0490c316a4276cd2f04221145
valeriiia2003 <leraspasibukhova@ukr.net>
2025-08-26 12:36:01 +0100

    Add StartCommandHandler for a '/start' users command

---
+++ TelegramBot.Infrastructure/Commands/StartCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Threading;
+using TelegramBot.Application.Interfaces;
+using TelegramBot.Infrastructure.Services;
+
+namespace TelegramBot.Infrastructure.Commands;
+
+public class StartCommandHandler : CommandHandler
+{
+    public override string Command => "/start";
+
+    public StartCommandHandler(IMessageSender _messageSender) : base(_messageSender) { }
+
+    public override Task HandleAsync(long chatId, string message, CancellationToken ct)
+    {
+        return _messageSender.SendTextAsync(chatId, "👋 Hello! I'm your bot.", ct);
+    }
+}

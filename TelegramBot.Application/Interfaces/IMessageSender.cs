4379d9539d0d6b02586395acc5f395d4b21be834
valeriiia2003 <leraspasibukhova@ukr.net>
2025-08-26 11:29:26 +0100

    Add IMessageSender to interact with Telegram API

---
+++ TelegramBot.Application/Interfaces/IMessageSender.cs
@@ -0,0 +1,14 @@
+﻿using Telegram.Bot.Types.Enums;
+namespace TelegramBot.Application.Interfaces;
+
+public interface IMessageSender
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="chatId">The chat ID to send the message to.</param>
+    /// <param name="message">The message to send.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task SendTextAsync(long chatId, string message, CancellationToken cancellationToken, ParseMode parse = ParseMode.None);
+}

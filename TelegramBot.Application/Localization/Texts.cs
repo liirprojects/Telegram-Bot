3d900652b8a6f2c6c8bf8d85b6a868fca56d38f1
valeriiia2003 <leraspasibukhova@ukr.net>
2025-08-28 14:19:09 +0100

    Add Texts.cs for all general commands

--- TelegramBot.Application/Localization/Texts.cs
+++ TelegramBot.Application/Localization/Texts.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using TelegramBot.Application.Localization;
+
+namespace TelegramBot.Application.Localization; 
+public static class Texts 
+{ 
+    public const string Unknown = "❓ Unknown command." +
+        " Type /help to see available commands.";
+
+    public const string Start = "Hi! I'm your bot. Type /help to see what I can do.";
+    public const string Help = "Available commands:\n/start — greet\n/help — show this help";
+
+}

23a8fe85af296a50753a9b42c723f6d6936061b9
valeriiia2003 <leraspasibukhova@ukr.net>
2025-08-28 14:22:34 +0100

    Add HelpCommandHandler for '/help' command

---
+++ TelegramBot.Infrastructure/Commands/HelpCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using TelegramBot.Application.Interfaces;
+using TelegramBot.Infrastructure.Services;
+using TelegramBot.Application.Localization;
+
+namespace TelegramBot.Infrastructure.Commands;
+
+public class HelpCommandHandler : CommandHandler
+{ 
+    public override string Command => "help";
+
+    public HelpCommandHandler(IMessageSender _messageSender) : base(_messageSender) { }
+
+    public override Task HandleAsync(long chatId, string message, CancellationToken ct)
+    {
+        return _messageSender.SendTextAsync(chatId, Texts.Help, ct);
+    }
+}

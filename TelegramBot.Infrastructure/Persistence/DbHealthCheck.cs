67c197d9abd43e97c36ecdfa62802fd2dbaf42a7
valeriiia2003 <leraspasibukhova@ukr.net>
2025-09-17 15:51:27 +0100

    Implement DB connection checker

---
+++ TelegramBot.Infrastructure/Persistence/DbHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+using TelegramBot.Application.Interfaces;
+namespace TelegramBot.Infrastructure.Persistence;
+
+// Tests and checks the connection with SQL SERVER
+public sealed class DbHealthCheck
+{
+    private readonly ISqlConnectionFactory _connectionFactory;
+
+    public DbHealthCheck(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _connectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<bool> CanConnectAsync(CancellationToken ct = default)
+    {
+        try
+        {
+            await using DbConnection conn = _connectionFactory.Create();
+            await conn.OpenAsync(ct);
+
+            await using var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT 1";
+
+            var x = await cmd.ExecuteScalarAsync(ct);
+
+            return x is int i && i == 1;
+        }
+        catch { return false; }
+    }
+}

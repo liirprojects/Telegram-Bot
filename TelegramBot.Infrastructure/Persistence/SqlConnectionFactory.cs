7aa7798bc409af2ccac2201dafe81439d0ad9938
valeriiia2003 <leraspasibukhova@ukr.net>
2025-09-17 13:08:14 +0100

    Add missing SqlConnectionFactory.cs in Persistence

---
+++ TelegramBot.Infrastructure/Persistence/SqlConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TelegramBot.Application.Interfaces;
+
+namespace TelegramBot.Infrastructure.Persistence;
+
+// Get connection string from appsettings.json and user secter to create SqlConnection
+public sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly string _cs;
+
+    public SqlConnectionFactory(IConfiguration cfg)
+    {
+        _cs = cfg.GetConnectionString("Default")
+            ?? throw new InvalidOperationException("ConnectionStrings:Default is missing");
+    }
+
+    public DbConnection Create() => new SqlConnection(_cs);
+}
